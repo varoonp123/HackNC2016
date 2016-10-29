4652eef8a415d5b9bb491d31114bc464fa9c071a
Bright <bright@dhcp-v80-101771.wireless.unc.edu>
2016-10-29 14:31:41 -0400

    Update routes

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var PythonShell = require('python-shell');
+
+app.use(bodyParser.json());  
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // respond with "hello world" when a GET request is made to the homepage
 
 app.set('view engine', 'ejs');
+
+app.get('*', function(req, res) {
+  console.log(req);
+  next();
+});
 
 app.get('/', function(req, res) {
   res.render('index.ejs');
 });
 
 app.use(express.static(__dirname + '/'));
+
+// PythonShell.run('my_script.py', function (err) {
+//   if (err) throw err;
+//   console.log('finished');
+// });
+
+
 
 app.listen(80);
